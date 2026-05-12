2753d83d23df08ba937222bbc1afe1f4cd976405
diegoydone-coder <diego.ydone@utec.edu.pe>
2026-05-11 22:42:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,26 @@ function obtenerMensaje() {
   return "¡Hola Mundo desde Node.js en la nube!";
 }
 
+function obtenerHora() {
+  const ahora = new Date();
+  return ahora.toLocaleString("es-PE", {
+    timeZone: "America/Lima"
+  });
+}
+
 app.get("/", (req, res) => {
-  res.send(obtenerMensaje());
+  res.send(`
+    ${obtenerMensaje()}<br>
+    Hora actual: ${obtenerHora()}
+  `);
 });
 
-app.get("/health", (req, res) => res.status(200).send("ok"));
+app.get("/health", (req, res) => {
+  res.status(200).send("ok");
+});
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor escuchando en puerto ${PORT}`);
+  console.log(Servidor escuchando en puerto ${PORT});
 });
